5166acf7c2f89f66a5e3820f8666d23efc1e8c70
ItSick <salesmen1@gmail.com>
2024-08-20 12:49:14 +0300

    model

--- Models/CountryModel.cs
+++ Models/CountryModel.cs
@@ -4,6 +4,13 @@ namespace CountriesApi.Models
     {
         public string Name { get; set; }
         public string Capital { get; set; }
-        public int Population { get; set; }
+        public int Flag { get; set; }
+        public Currency[] Currencies {get; set;}
+    }
+
+    public class Currency {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string Symbol {get; set;}
     }
 }
